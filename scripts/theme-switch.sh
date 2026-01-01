d22e380b48aed72a92fd82ee31c24422e42ef053
JwpAT <150574239+JwpAT@users.noreply.github.com>
2026-01-01 11:55:54 -0500

    Delete scripts directory

--- scripts/theme-switch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "include $HOME/.config/kitty/themes/$1.conf" > "$HOME/.config/kitty/kitty.conf"
-echo "source = $HOME/.config/hypr/themes/$1.conf" > "$HOME/.config/hypr/theme.conf"
-quickshell ipc call theme themeSet $1
-hyprctl reload
-pkill -SIGUSR1 kitty
